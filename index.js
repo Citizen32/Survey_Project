93da0793c1660cfda2bd45b480a00584b73f1e1e
David Morales <morgar.creative@gmail.com>
2018-02-03 14:42:13 -0600

    google authenticate works

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys')
 
-
 const app = express();
 
+// TEST ROUTE ==============
+// app.get('/', (req, res) => {
+//     res.send({
+//         bye: 'Hello World!'
+//     });
+// });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+        console.log(accessToken);
+    })
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
     })
 );
 
