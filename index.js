7ba62d67ac6453f62b2bd28a37b76427fe8de131
David Morales <morgar.creative@gmail.com>
2018-02-04 13:20:25 -0600

    Server structure refactor

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
+require('./services/passport');
 
 const app = express();
 
-// TEST ROUTE ==============
-// app.get('/', (req, res) => {
-//     res.send({
-//         bye: 'Hello World!'
-//     });
-// });
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile); 
-    })
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get(
-    '/auth/google/callback/',
-    passport.authenticate('google')
-);
+require('./routes/authRoutes')(app);
 
 // THIS CONST SPECIFIES WHERE OUR APPLICATION IS RUNNING, EITHER HEROKU'S ENVIROMENT (PRODUCTION) PORT OR LOCAL HOST (DEVELOPMENT) 
 const PORT = process.env.PORT || 8080;
